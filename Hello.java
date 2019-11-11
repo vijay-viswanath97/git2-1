f681c07af9a887159ab44ff32895acaa3c642893
Anjuna661 <57387984+Anjuna661@users.noreply.github.com>
2019-11-11 09:59:40 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -18,8 +18,7 @@ public class Hello{
 			System.out.println("Thanks for the webhooks demonstration 13..");
 			System.out.println("Thanks for the webhooks demonstration 14..");
 			System.out.println("Thanks for the webhooks demonstration 15..");
-			System.out.println("Thanks for the webhooks demonstration 16..");
-			System.out.println("Thanks for the webhooks demonstration 17..");
+			System.out.println("Thanks for the webhooks demonstration 1617..");
 			
 		}
 	}
